86edc7fc3902ede57045ed5339a00cbda084bd8c
KAMLESH NISHAD <142823002+kamlesh079@users.noreply.github.com>
2026-05-29 15:53:54 +0530

    version 1

--- Step 2: Learn Important Sorting Techniques/38.cpp
+++ Step 2: Learn Important Sorting Techniques/38.cpp
@@ -1,1 +1,27 @@
 // https://www.geeksforgeeks.org/problems/quick-sort/1
+class Solution {
+  public:
+    void quickSort(vector<int>& arr, int low, int high) {
+       if(low < high){
+           int pId = partition(arr, low, high);
+           quickSort(arr, low, pId - 1);
+           quickSort(arr, pId + 1, high);
+       }
+    }
+
+  public:
+    int partition(vector<int>& arr, int low, int high) {
+        int pivot = arr[high];
+        
+        int i = low - 1;
+        for(int j = low; j < high; j++){
+            if(arr[j] < pivot){
+                i++;
+                swap(arr[j], arr[i]);
+            }
+        }
+        // swap(arr[i+1], pivot); // pivot stores only the value, not the actual array element, so swap with arr[high], not pivot.
+        swap(arr[i+1], arr[high]);
+        return i + 1;
+    }
+};
