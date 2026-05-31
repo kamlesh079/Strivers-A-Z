06907c1e20eaf609727e15a7a10a9d0c2461ab42
KAMLESH NISHAD <142823002+kamlesh079@users.noreply.github.com>
2026-05-31 20:45:57 +0530

    BS Question

--- Step 4: Binary Search [1D, 2D Arrays, Search Space]/98.cpp
+++ Step 4: Binary Search [1D, 2D Arrays, Search Space]/98.cpp
@@ -1,3 +1,20 @@
 // https://leetcode.com/problems/kth-missing-positive-number/description/
 
 // # Using BS(Binary Search Method)
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int low = 0;
+        int high = arr.size() - 1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int missing = arr[mid] - (mid + 1);
+            
+            if(missing < k) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return low + k;
+    }
+};
