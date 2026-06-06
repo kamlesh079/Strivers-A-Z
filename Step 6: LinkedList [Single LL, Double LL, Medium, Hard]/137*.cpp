c9058f7987b39c56429ff38cfff38d14b2d74b3f
KAMLESH NISHAD <142823002+kamlesh079@users.noreply.github.com>
2026-06-06 19:58:22 +0530

    Reverse LinkedList

--- Step 6: LinkedList [Single LL, Double LL, Medium, Hard]/137*.cpp
+++ Step 6: LinkedList [Single LL, Double LL, Medium, Hard]/137*.cpp
@@ -1,4 +1,39 @@
 // https://leetcode.com/problems/reverse-linked-list/
 
+// # 1st Way
+class Solution {
+public:
+    ListNode* reverseHead(ListNode* pre, ListNode* curr){
+        if(curr == NULL) return pre;
 
+        ListNode* nextNode = curr -> next;
+        curr -> next = pre;
+        pre = curr;
+        curr = nextNode;
+
+        return reverseHead(pre, curr);
+
+    }
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre = NULL;
+        ListNode* curr = head;
+        return reverseHead(pre, curr);
+    }
+};
+
+// # 2nd Way
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head -> next) return head;
+
+        ListNode* newHead = reverseList(head -> next);
+        
+        head -> next -> next = head;
+        head -> next = NULL;
+
+        return newHead;
+
+    }
+};
 
